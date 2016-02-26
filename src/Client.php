52295d3133a673fe1caffbb56da5823fca286de9
Ryan Brown <browneryan87@gmail.com>
2016-02-26 13:31:21 -0800

    Add passing getClientName test.

--- src/Client.php
+++ src/Client.php
@@ -1,14 +1,22 @@
 <?php
-	//  class Client
-		// {
-		// private property;
-		//
-		//  function getProperty(){
-		// 	return $this->property;
-		// 	}
-		//
-		// function setProperty(){
-		// 	$this->property = $property;
-		// }
-	// }
+    class Client
+    {
+        private $client_name;
+        private $id;
+        private $stylist_id;
+
+        function __construct($client_name, $id = null, $stylist_id)
+        {
+            $this->client_name = $client_name;
+            $this->id = $id;
+            $this->stylist_id = $stylist_id;
+        }
+
+        function getClientName()
+    	{
+            return $this->client_name;
+        }
+
+    }
+
  ?>
--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -1,22 +1,33 @@
 <?php
 
-	// require_once 'src/Client.php';
+	/**
+	* @backupGlobals disabled
+	* @backupStaticAttributes disabled
+	*/
 
-	// class ClientTest extends PHPUnit_Framework_TestCase
-	// {
+	require_once 'src/Client.php';
+	require_once 'src/Stylist.php';
 
-		// function test_makeTitleCase_oneWord()
-		// {
-		// //Arrange
-		// $test_TitleCaseGenerator = new TitleCaseGenerator;
-		// $input = 'beowulf';
-		//
-		// //Act
-		// $result = $test_TitleCaseGenerator->makeTitleCase($input);
-		//
-		// //Assert
-		// $this->assertEquals('Beowulf', $result);
-		// }
-	// }
+	$server = 'mysql:host=localhost;dbname=hair_salon_test';
+	$username = 'root';
+	$password = 'root';
+	$DB = new PDO($server, $username, $password);
+
+	class ClientTest extends PHPUnit_Framework_TestCase
+	{
+		function test_getClientName()
+        {
+            //Arrange
+            $name = "Ralph";
+			$stylist_id = 1;
+            $test_Client = new Client($name, $id = null, $stylist_id);
+
+            //Act
+            $result = $test_Client->getClientName();
+
+            //Assert
+            $this->assertEquals($name, $result);
+        }
+	}
 
 ?>
